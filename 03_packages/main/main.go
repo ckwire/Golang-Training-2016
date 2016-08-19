1e8ef5bfdc99e162de6338ccb87b98c9d5651c0a
ckwire <ckwire@gmail.com>
2016-08-18 21:28:42 -0400

    Created 03_package example. Adds main/main.go, stringutil folder with name.go & reverse.go

---
+++ 03_packages/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ckwire/GolangTraining/03_packages/stringutil"
+)
+
+func main() {
+	//fmt.Println(stringutil.Reverse("!oG , olleH"))
+	fmt.Println(stringutil.MyName)
+}
---
+++ 03_packages/stringutil/name.go
@@ -0,0 +1,4 @@
+package stringutil
+
+// having var name being Capitalized makes it Exported, accessible outside from main.go
+var MyName = "Chad"
